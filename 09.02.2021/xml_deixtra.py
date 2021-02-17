5fbff037a231e0b837a2035f39f6aa2127fe9e6b
Legellon <>
2021-02-17 03:07:15 +0200

    1

--- 09.02.2021/xml_deixtra.py
+++ 09.02.2021/xml_deixtra.py
@@ -1,46 +1,65 @@
 import xml.etree.ElementTree as ET
-
-pointsTo = [1, 5]
-points_routes = [[],[]]
+import math
 
 postMap = ET.parse("map.xml")
 postMapRoot = postMap.getroot()
 
-for point in postMapRoot[0]:
-    points_routes[0].append(point.attrib)
+pointsWithRoutes = {}
 
-matrix_of_distances = [[0] * len(points_routes[0]) for i in range(len(points_routes[0]))]
+for point in postMapRoot[0]:
+    prop = {'x': float(point.attrib['x']), 'y': float(point.attrib['y'])}
+
+    pointsWithRoutes.update({point.attrib['id']: {'prop': prop, 'routes': []} })
+
+COUNT_OF_POINTS = len(pointsWithRoutes.keys())
+
+matrixOfWeights = [[0] * COUNT_OF_POINTS for currentPoint in range(COUNT_OF_POINTS)]
 
 for route in postMapRoot[1]:
-    points_routes[1].append(route.attrib)
-    matrix_of_distances[int(route.attrib['from']) - 1][int(route.attrib['to']) - 1] = int(route.attrib['cfd'])
+    _distance = math.sqrt(pow(pointsWithRoutes[route.attrib['from']]['prop']['x'] - pointsWithRoutes[route.attrib['to']]['prop']['x'], 2) + pow(pointsWithRoutes[route.attrib['from']]['prop']['y'] - pointsWithRoutes[route.attrib['to']]['prop']['y'], 2))
+    _vmax = float(route.attrib['vmax'])
 
-is_used = [False] * len(points_routes[0])
-distances_to_point = [math.inf] * len(points_routes[0])
+    new_route = {'to': route.attrib['to'], 'weight': _distance / _vmax}
+    pointsWithRoutes[route.attrib['from']]['routes'].append(new_route)
+
+    matrixOfWeights[int(route.attrib['from'])-1][int(route.attrib['to'])-1] = new_route['weight']
+
+used_point = [False] * COUNT_OF_POINTS
+distance = [math.inf] * COUNT_OF_POINTS
+previous_point = [None] * COUNT_OF_POINTS
+path_fromStart = []
+
+START_POINT = 8
+END_POINT = 1
 
 minimal_distance = 0
-minimal_point = pointsTo[0] - 1
+minimal_point = START_POINT - 1
 
-distances_to_point[minimal_point] = 0
+distance[minimal_point] = 0
 
 while minimal_distance < math.inf:
-    i = minimal_point
-    is_used[i] = True
+    currentPoint = minimal_point
+    used_point[currentPoint] = True
 
-    near_routes = []
-    for route in postMapRoot[1]:
-        if(route.attrib['from'] == i): 
-            near_points.append(int(point.attrib['to']) - 1)
+    for near_availablePoint in pointsWithRoutes[str(currentPoint + 1)]['routes']:
+        near_availablePoint = int(near_availablePoint['to']) - 1
 
-    for j in near_routes:
-        if distances_to_point[i] + matrix_of_distances[i][j] < distances_to_point[j]:
-            distances_to_point[j] = distances_to_point[i] + 
+        if distance[currentPoint] + matrixOfWeights[currentPoint][near_availablePoint] < distance[near_availablePoint]:
+            distance[near_availablePoint] = distance[currentPoint] + matrixOfWeights[currentPoint][near_availablePoint]
+
+            previous_point[near_availablePoint] = currentPoint + 1
 
     minimal_distance = math.inf
 
-    for i in range(len(points_routes[0])):
-        if not used[i] and distances_to_point[i] < minimal_distance:
-            minimal_distance = distances_to_point[i]
-            minimal_point = i
-    
-    
+    for currentPoint in range(COUNT_OF_POINTS):
+        if not used_point[currentPoint] and distance[currentPoint] < minimal_distance:
+            minimal_distance = distance[currentPoint]
+            minimal_point = currentPoint
+
+while END_POINT is not None:
+    path_fromStart.insert(0, END_POINT)
+    END_POINT = previous_point[END_POINT - 1]
+
+print(distance)
+print(path_fromStart)
+#print(path_fromStart)
