89d1f4864fa0b9fe44547292eff55f6effcd01bd
Legellon <>
2021-02-25 00:18:02 +0200

    1

--- 09.02.2021/ttt.py
+++ 09.02.2021/ttt.py
@@ -1,5 +1,5 @@
-newRoute = [[[1, 2], 0],[[1, 2], 0],[[1, 2], 0]]
+a = [[[1, 2], 3], [[3, 4], 3], [[5, 6], 3], [[7, 8], 3]]
 
+x = [[1, 2] in route for route in a].count(True) > 0
 
-
-print(newRoute)
+print(x)
